32906c781d3027f4fb6e59383afdebfe2e677c47
Ofri Kirshen <okirshen@gmail.com>
2023-08-08 15:24:48 +0300

    Updated aseprite

--- overlays.nix
+++ overlays.nix
@@ -1,7 +1,7 @@
 [
   (self: super: {
     aseprite-unfree = super.aseprite-unfree.overrideAttrs (attrs: {
-      version = "1.3-beta21";
+      version = "1.3-rc6";
       postPatch = "";
     });
   })
