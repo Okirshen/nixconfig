6bb4506419e5e01b14969892312ad7cef750e87c
Ofri Kirshen <okirshen@gmail.com>
2023-08-11 23:16:27 +0300

    added nix-community cachix

--- common/configuration.nix
+++ common/configuration.nix
@@ -1,7 +1,16 @@
 { pkgs, primaryUser, machineName, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    experimental-features = [ "nix-command" "flakes" ];
+  };
 
   users.users.${primaryUser} = {
     isNormalUser = true;
