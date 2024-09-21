0757d2e7b43e430f99bf29cdae4f980b8197c53e
Ofri Kirshen <okirshen@proton.me>
2024-09-21 03:58:33 +0300

    FIX: wezterm broken font rendering

--- modules/shell/wezterm/wezterm.lua
+++ modules/shell/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 -- local wezterm = require 'wezterm'
 local config = {
   color_scheme = 'Dracula (base16)',
-  enable_wayland = false
+  enable_wayland = true,
+  -- fixes broken font rendering
+  front_end = 'WebGpu',
 }
 return config
